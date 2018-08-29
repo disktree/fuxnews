79c70069bc0b3e706554bd65b8b3bbbaf4068f7b
tong <tong@disktree.net>
2018-08-29 11:32:20 +0200

    Cleanup

--- src/fux/App.hx
+++ src/fux/App.hx
@@ -1,5 +1,6 @@
 package fux;
 
+import js.Browser.console;
 import js.Browser.document;
 import js.Browser.window;
 import js.html.Element;
@@ -142,57 +143,47 @@ class App {
 		}
 	];
 
-	static var current : Int;
 	static var video : VideoElement;
-
-	static function loadVideo( name : String ) {
-		video.src = 'video/$name.mp4';
-		video.currentTime = Math.random() * 3;
-		//video.playbackRate = 4;
-	}
-
-	static inline function loadLiveVideo( name : String ) {
-		loadVideo( 'live/$name' );
-	}
-
-	static function loadChannel( index : Int ) {
-
-		if( index < 0 ) index = 0;
-		else if( index >= playlist.length ) index = playlist.length;
-
-		if( current == null ) {
-			//
-		}
-
-		current = index;
-
-		var channel = playlist[current];
-		if( channel.index == null ) channel.index = 0 else {
-			if( ++channel.index >= channel.video.length ) channel.index = 0;
-		}
-
-		var file = channel.video[channel.index];
-		loadLiveVideo( file );
-	}
+	static var channel : Channel;
+	static var index : Int; // Channel index
 
 	static function loadNextChannel() {
-		if( ++current >= playlist.length ) current = 0;
-		loadChannel( current );
+		if( ++index >= playlist.length-1 ) index = 0;
+		channel = playlist[index];
+		loadVideo();
 	}
 
 	static function loadPrevChannel() {
-		current = (current == 0) ? playlist.length-1 : current-1;
-		loadChannel( current );
+		if( --index < 0 ) index = playlist.length-1;
+		channel = playlist[index];
+		loadVideo();
+	}
+
+	static function loadNextVideo() {
+		if( channel.index >= channel.video.length-1 ) loadNextChannel();
+		else {
+			channel.index++;
+			loadVideo();
+		}
+	}
+
+	static function loadVideo() {
+		var name = channel.video[channel.index];
+		console.log(name);
+		video.src = 'video/live/$name.mp4';
+		video.currentTime = Math.random() * 2; // Slighty random start time offset
+		//video.playbackRate = 8;
 	}
 
 	static function main() {
 
 		window.onload = function(){
 
+			console.info('FuxNews');
+
 			video = cast document.getElementById( 'live' );
 			video.muted = true;
 			video.autoplay = true;
-			//video.loop = true;
 			/*
 			video.addEventListener( 'loadstart', function(e){
 				//background.play();
@@ -205,9 +196,7 @@ class App {
 			}, false );
 			*/
 			video.addEventListener( 'ended', function(e){
-				var channel = playlist[current];
-				if( ++channel.index >= channel.video.length ) channel.index = 0;
-				loadLiveVideo( channel.video[channel.index] );
+				loadNextVideo();
 			}, false );
 
 			/*
@@ -220,12 +209,19 @@ class App {
 			} else {
 				index = Std.parseInt( stateItem );
 			}
-			trace(index);
 			//var state = (stateItem == null) ? 0 : Std.parseInt( stateItem );
 			*/
+
+			for( ch in playlist ) {
+				ch.index = 0;
+				ch.video.shuffle();
+			}
+
 			playlist.shuffle();
-			var index = Std.int( Math.random() * (playlist.length-1) );
-			loadChannel( index );
+
+			index = 0;
+			channel = playlist[index];
+			loadVideo();
 
 			document.body.onclick = e -> {
 				if( video.readyState == 4 ) loadNextChannel();
@@ -234,6 +230,7 @@ class App {
 				e.preventDefault();
 				if( video.readyState == 4 )  loadPrevChannel();
 			}
+
 			/*
 			window.onbeforeunload = e -> {
 				if( current != null ) storage.setItem( 'fuxnews', Std.string(current) );
